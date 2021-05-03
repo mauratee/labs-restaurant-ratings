51076ca5d5f7f9f3c9335f2a92d9b31013bb6d74
Maura Thompson <thomaura@gmail.com>
2021-05-03 21:48:13 +0000

    Wow! Finally finished sorting and printing
    Co-authored by Crystal Anderson <Crystal Anderson <cryssann@gmail.com>

--- ratings.py
+++ ratings.py
@@ -7,19 +7,39 @@ open_file = open("scores.txt")
 # read lines of file
 def ordered_ratings(open_file):
     ratings = {}
+    ratings_list = []
+    # ratings_list = ratings_list.sort()
+
 # break each line into parts, whitestrip, maybe split by colon?
     for line in open_file:
         line = line.rstrip()
         words = line.split(":")
         ratings[words[0]] = words[1]
-        rest_name, rating = ratings
-        print(f"{rest_name} is rated at {rating}.")
+        ratings_list.append(f"{words[0]} is rated at {words[1]}.")
+    
+    #ratings_list = ratings_list.sort()
+    sorted_ratings_list = sorted(ratings.items())
+
+    for restaurant in sorted_ratings_list:
+        print(f"{restaurant[0]} is rated at {restaurant[1]}.")
+    #for rate in ratings_list:
+    #    print(rate)
+
     #print(sorted(ratings))
-    return (sorted(ratings.items(print(f"{rest_name} is rated at {rating}."))))
+    # print(sorted_ratings_list)
+    #print(type(ratings_list))
+    #print(ratings_list)
+    #print(ratings_list.sort())
+
+    #return ratings_list.sort()
+    #return (sorted(ratings.items()))
+
+
+
     
 # create dictionary (blank)
 # use dict. get() to add key value pairs to dictionary, restaurant name is key
 # put dictionary keys in alpha order
 
 test = ordered_ratings(open_file)
-print(test)
+#print(test)
